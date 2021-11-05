56023f3faf96a9acdbda068725a04baf7b51fde3
Trac3D1y <i2rys@protonmail.com>
2021-11-05 12:09:37 +0800

    Create index.py

---
+++ index.py
@@ -0,0 +1,33 @@
+#Dependencies
+import cv2
+
+#Variables
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+
+cap = cv2.VideoCapture(0)
+
+#Main
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+
+    cv2.imshow("img", img)
+
+    if cv2.waitKey(1) & 0xff == ord("q"):
+        print("OpenCV has been stopped.")
+        break
+
+cap.release()
+cv2.destroyAllWindows()
